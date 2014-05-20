d84d56d5de9899117bedac35fa41bacea4b05c0d
edi9999 <hipp.edg@gmail.com>
2014-05-20 18:54:18 +0200

    upload sample document to S3 works

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-var request=require('request');
+var request=require('request'),
+	fs=require('fs'),
+	path=require('path');
 var key="a457f87f54a654a87fd89aeff"
 
 endpoint="http://localhost:3000/";
-request.post(endpoint+"api/templates/?key="+key, function callback (err, httpResponse, body) {
+r=request.post(endpoint+"api/templates/?key="+key, function callback (err, httpResponse, body) {
   if (err) {
     return console.error('upload failed:', err);
   }
   console.log('Upload successful!  Server responded with:', body);
 });
+
+var form = r.form()
+form.append('file', fs.createReadStream(path.join(__dirname, 'sample.docx')))
